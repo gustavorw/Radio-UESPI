e525dcea208457ab599575eff19eb7ffe2d6336e
DevGustavo <46036174+GustavoDev17@users.noreply.github.com>
2019-11-02 01:54:59 -0300

    add api index.php

---
+++ index.php
@@ -0,0 +1,168 @@
+<?php
+    $data = array(
+        array(
+            'titulo' => 'III Seminário Regional de Educação acontece entre os dias 14 e 16 de outubro em Corrente',
+            'data' => "11\nout",
+            'link' => 'https://www.uespi.br/site/?p=127240'
+        ),
+        array(
+            'titulo' => 'Comunicado – Dia do Professor',
+            'data' => "10\nout",
+            'link' => 'https://www.uespi.br/site/?p=127236'
+        ),
+        array(
+            'titulo' => 'Inscrições para monitorias são prorrogadas até amanhã (11)',
+            'data' => "10\nout",
+            'link' => 'https://www.uespi.br/site/?p=127234'
+          
+        ),
+        array(
+            'titulo' => 'Alunos de Ed. Física de Picos promovem I Gincana Acadêmica de Jogos e Brincadeiras Tradicionais',
+            'data' => "10\nout",
+            'link' => 'https://www.uespi.br/site/?p=126759'
+          
+        ),
+        array(
+            'titulo' => 'Em audiência na Alepi, deputados estaduais discutem ações para a resolução de demandas da IES',
+            'data' => "09\nout",
+            'link' => 'https://www.uespi.br/site/?p=127162'
+          
+        ),
+        array(
+            'titulo' => 'PREX divulga edital para seleção de bolsista no PIBEU',
+            'data' => "09\nout",
+            'link' => 'https://www.uespi.br/site/?p=127182'
+          
+        ),
+        array(
+            'titulo' => 'Estudantes da UESPI são convidados a participar do projeto 70 olhares sobre Direitos Humanos',
+            'data' => "09\nout",
+            'link' => 'https://www.uespi.br/site/?p=127142'
+          
+        ),
+        array(
+            'titulo' => 'PREX convoca classificado do edital de Teatro',
+            'data' => "09\nout",
+            'link' => 'https://www.uespi.br/site/?p=127149'
+          
+        ),
+        array(
+            'titulo' => 'UESPI divulga edital de monitoria remunerada e não remunerada com 875 vagas',
+            'data' => "08\nout",
+            'link' => 'https://www.uespi.br/site/?p=127138'
+          
+        ),
+        array(
+            'titulo' => 'Projeto da UESPI utiliza o cinema como ferramenta de ensino',
+            'data' => "08\nout",
+            'link' => 'https://www.uespi.br/site/?p=127081' 
+        ),
+        array(
+            'titulo' => 'Residentes realizam Seminário de Formação em Saúde do Idoso',
+            'data' => "08\nout",
+            'link' => 'https://www.uespi.br/site/?p=127092' 
+        ),
+        array(
+            'titulo' => 'UESPI divulga nona chamada da Lista de Espera do SISU',
+            'data' => "08\nout",
+            'link' => 'https://www.uespi.br/site/?p=127127' 
+        ),
+        array(
+            'titulo' => 'Grupo de Direito de Bom Jesus discute cotas e previdência social em Simpósio de Produção Científica',
+            'data' => "08\nout",
+            'link' => 'https://www.uespi.br/site/?p=127020' 
+        ),
+        array(
+            'titulo' => 'II JECB inicia programação com palestra sobre educação científica',
+            'data' => "08\nout",
+            'link' => 'https://www.uespi.br/site/?p=127073' 
+        ),
+        array(
+            'titulo' => 'VIII Semana de Matemática discute o desenvolvimento da área ao longo dos anos',
+            'data' => "07\nout",
+            'link' => 'https://www.uespi.br/site/?p=127019' 
+        ),
+        array(
+            'titulo' => 'Empresa Júnior de Turismo abre seleção para o programa de Trainee',
+            'data' => "07\nout",
+            'link' => 'https://www.uespi.br/site/?p=127052' 
+        ),
+        array(
+            'titulo' => 'II Semana da História reúne discussões sobre Patrimônio e Cultura de Floriano',
+            'data' => "07\nout",
+            'link' => 'https://www.uespi.br/site/?p=127018' 
+        ),
+        array(
+            'titulo' => 'Comunicado',
+            'data' => "05\nout",
+            'link' => 'https://www.uespi.br/site/?p=127015' 
+        ),
+        array(
+            'titulo' => 'Comunicado',
+            'data' => "05\nout",
+            'link' => 'https://www.uespi.br/site/?p=127015' 
+        ),
+        array(
+            'titulo' => 'Nota de pesar',
+            'data' => "05\nout",
+            'link' => 'https://www.uespi.br/site/?p=127013' 
+        ),
+        array(
+            'titulo' => '90% dos cursos que realizaram o ENADE obtiveram nota maior ou igual a média',
+            'data' => "04\nout",
+            'link' => 'https://www.uespi.br/site/?p=126953' 
+        ),
+        array(
+            'titulo' => 'Programa de Acompanhamento de Egressos coleta dados para fortalecer o ensino na UESPI',
+            'data' => "04\nout",
+            'link' => 'https://www.uespi.br/site/?p=126964' 
+        ),
+        array(
+            'titulo' => 'Inscrições abertas para V Semana de Letras do campus Prof. Possidônio Queiroz',
+            'data' => "04\nout",
+            'link' => 'https://www.uespi.br/site/?p=126960' 
+        ),
+        array(
+            'titulo' => 'PREX divulga resultado final da fase de documentação do PIBEU',
+            'data' => "04\nout",
+            'link' => 'https://www.uespi.br/site/?p=126950' 
+        ),   
+        array(
+            'titulo' => 'Extensão Universitária atua no combate ao mosquito Aedes Aegypti em Parnaíba',
+            'data' => "03\nout",
+            'link' => 'https://www.uespi.br/site/?p=126754' 
+        ),
+        array(
+            'titulo' => 'Projeto da UESPI trabalha no acolhimento de crianças e adolescentes vítimas de violência sexual',
+            'data' => "03\nout",
+            'link' => 'https://www.uespi.br/site/?p=126863' 
+        ),
+        array(
+            'titulo' => 'PREG divulga Errata II do edital de Transferência Externa',
+            'data' => "03\nout",
+            'link' => 'https://www.uespi.br/site/?p=126936' 
+        ),
+        array(
+            'titulo' => 'UNATI participa de evento nacional em Aracaju',
+            'data' => "03\nout",
+            'link' => 'https://www.uespi.br/site/?p=126885' 
+        ),
+        array(
+            'titulo' => 'PIBID reabre processo seletivo para subprojeto em Parnaíba',
+            'data' => "03\nout",
+            'link' => 'https://www.uespi.br/site/?p=126893' 
+        ),
+        array(
+            'titulo' => 'PREG divulga Errata I do edital de Transferência Externa',
+            'data' => "02\nout",
+            'link' => 'https://www.uespi.br/site/?p=126879' 
+        ),
+        array(
+            'titulo' => 'Campus de Parnaíba realiza I Semana de Ciências Sociais',
+            'data' => "02\nout",
+            'link' => 'https://www.uespi.br/site/?p=126861' 
+        )
+    );
+        header("Content-type:application/json"); 
+        echo(json_encode($data, true));
+?>
